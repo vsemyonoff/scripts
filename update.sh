01036769584def5fdad8ad328b9b7de3b1fe6fea
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-01-11 15:39:24 +0200

    Gentoo update script fixed

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ source /etc/profile && \
 
 locale-gen && \
 emerge --sync && \
-emerge -u portage && \
-emerge -u linux-headers glibc binutils gcc-config gcc && \
+emerge portage && \
+emerge linux-headers glibc binutils gcc-config gcc && \
 binutils-config $(binutils-config -l | wc -l) && \
 gcc-config $(gcc-config -l | wc -l) && \
 source /etc/profile && \
-emerge -bu glibc binutils gcc portage && \
+emerge -b glibc binutils gcc portage && \
 echo "Toolchain update complete." > upd-"`date +%Y%m%d`".log
 
 emerge -bke system && \
