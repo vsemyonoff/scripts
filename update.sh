0f1eeef2669dd5a0e1bfa6e98108537ba281ca3f
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-01-11 16:12:55 +0200

    Gentoo update.sh: small fix

--- update.sh
+++ update.sh
@@ -13,9 +13,11 @@ source /etc/profile && \
 emerge -b glibc binutils gcc && \
 echo "Toolchain update complete." > upd-"`date +%Y%m%d`".log
 
+[ $? == 0 ] && \
 emerge -bke system && \
 emerge -bke world && \
 emerge --depclean && \
 echo "World update complete." >> upd-"`date +%Y%m%d`".log
 
-echo "Now execute 'dispatch-conf'..." >> upd-"`date +%Y%m%d`".log
+[ $? == 0 ] && \
+echo "Update complete. Use 'dispatch-conf' to update configs." >> upd-"`date +%Y%m%d`".log
