5b5e9d7549d5fb23b8584cdeae2f398fc3203ed4
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-01-11 16:09:56 +0200

    Gentoo update.sh fixed: revdep-rebuild call removed

--- update.sh
+++ update.sh
@@ -10,14 +10,12 @@ emerge linux-headers glibc binutils gcc-config gcc && \
 binutils-config $(binutils-config -l | wc -l) && \
 gcc-config $(gcc-config -l | wc -l) && \
 source /etc/profile && \
-emerge -b glibc binutils gcc portage && \
+emerge -b glibc binutils gcc && \
 echo "Toolchain update complete." > upd-"`date +%Y%m%d`".log
 
 emerge -bke system && \
 emerge -bke world && \
-emerge -bku gentoolkit && \
 emerge --depclean && \
-revdep-rebuild && \
 echo "World update complete." >> upd-"`date +%Y%m%d`".log
 
 echo "Now execute 'dispatch-conf'..." >> upd-"`date +%Y%m%d`".log
