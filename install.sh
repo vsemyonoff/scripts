d8167bdc1161b555237e21fb123fdedb070dd82f
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-02-11 00:33:11 +0200

    Gentoo install minor changes

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+[ -z $1 ] && echo "usage: $0 stage3" && exit 1
+
 GEN="/mnt/gentoo"
 
-#rm -fr ${GEN} && \
-#mkdir ${GEN} && \
-tar xvjpf stage3-amd64-20091231.tar.bz2 -C ${GEN} && \
+tar xvjpf "$1" -C ${GEN} && \
 tar xvjf portage-latest.tar.bz2 -C ${GEN}/usr && \
 mount -t proc none ${GEN}/proc && \
 mount -t sysfs none ${GEN}/sys && \
