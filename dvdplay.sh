eafd3507fe408fd95a23dbcd6bd938757ab56b35
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-02-16 18:22:04 +0200

    dvdplay.sh: fixed

--- dvdplay.sh
+++ dvdplay.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-    exec mplayer dvdnav:// -dvd-device "$(pwd)"
-else
-    exec mplayer dvdnav:// -dvd-device "$(readlink -m $1)"
-fi
+DEVICE="${1:-"$(pwd)"}"
+
+exec mplayer dvdnav:// -dvd-device "${DEVICE}"
