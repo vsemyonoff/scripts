85605b73ee6bc1c2a39d129c24e542788f3f8615
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-02-16 18:14:06 +0200

    Arch32 install fixed

--- mkarch32.sh
+++ mkarch32.sh
@@ -5,19 +5,17 @@ if [ "$UID" != "0" ]; then
     exit 1
 fi
 
-ARCH32="/opt/arch32"
+ARCH32=${1:-"/opt/arch32"}
 
-sed -e 's/x86_64/i686/g' /etc/pacman.d/mirrorlist > /etc/pacman.d/mirrorlist32
-sed -e 's@/etc/pacman.d/mirrorlist@/etc/pacman.d/mirrorlist32@g' /etc/pacman.conf > /etc/pacman32.conf
-mkdir -p $ARCH32/var/{cache/pacman/pkg,lib/pacman,lib/dbus}
+mkdir -p $ARCH32/etc/pacman.d || exit 1
+sed -e 's/x86_64/i686/g' /etc/pacman.d/mirrorlist > /tmp/mirrorlist
+sed -e 's@/etc/pacman.d/mirrorlist@/tmp/mirrorlist@g' /etc/pacman.conf > /tmp/pacman.conf
+mkdir -p $ARCH32/var/{cache/pacman/pkg,lib/pacman} || exit 1
 
-pacman --root $ARCH32                           \
-       --cachedir $ARCH32/var/cache/pacman/pkg  \
-       --config /etc/pacman32.conf -Sy filesystem \
-                                       grep       \
-                                       gawk       \
-                                       sed        \
-                                       procps     \
-                                       bash       \
-                                       coreutils  \
-                                       gzip
+pacman --root $ARCH32                          \
+       --cachedir $ARCH32/var/cache/pacman/pkg \
+       --config /tmp/pacman.conf -Sy base
+
+rm -f $ARCH32/etc/pacman.d/mirrorlist
+mv /tmp/mirrorlist $ARCH32/etc/pacman.d
+rm -f /tmp/pacman.conf
