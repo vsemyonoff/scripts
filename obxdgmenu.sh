fffed7fb41195ec80d88890d4d1cd2b37f65e584
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-03-19 17:56:38 +0200

    Cleanup performed

--- obxdgmenu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec xdg_menu --fullmenu --format openbox3-pipe --root-menu /etc/xdg/menus/arch-applications.menu
--- sshlog.sh
+++
@@ -1,7 +0,0 @@
-rm -f accepted.txt failed.txt
-FILES="auth.log auth.log.1 auth.log.2 auth.log.3 auth.log.4"
-
-for i in $FILES; do
-    grep "Accepted password" $i | tac >> accepted.txt
-    grep "Failed password" $i | tac >> failed.txt
-done
