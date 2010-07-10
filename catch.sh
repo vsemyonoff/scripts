063667c93e2313ddff7e6382b2639fe785661788
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-07-10 03:48:08 +0300

    Recorded video compression added

--- catch.sh
+++ catch.sh
@@ -4,4 +4,5 @@ if [ $# -ne 1 ]; then
     echo "usage: $0 output"
 fi
 
-screen -m -d mencoder -fps 7.52 tv:// -ovc copy -o "$1"; sleep 2; mplayer "$1"; screen -r
+screen -m -d mencoder -fps 7.52 tv:// -ovc lavc -lavcopts vcodec=mpeg4 -o "$1"; sleep 2; mplayer "$1"; screen -r
+#mencoder tv:// -tv driver=v4l2:width=320:height=240:device=/dev/video0:alsa:forceaudio:amode=0:adevice=hw.0,0 -ovc lavc -lavcopts vcodec=mpeg4 -oac mp3lame -lameopts vbr=3:br=32:mode=3 -af volnorm -o VideoFile.avi
