ff073160edb2a39e91f09deecd5008a5bd3d13a9
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-08-03 17:48:03 +0300

    Backlight and audio control scripts added

---
+++ audio-osd.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#
+#  File: audio-osd.sh
+#  Description:
+#
+#  Creation date: 2010.08.03 17:41:53
+#  Last modified: 2010.08.03 17:47:13
+#
+#  Copyright © 2010 Vladyslav Semyonoff <vsemyonoff@gmail.com>
+#
+
+OSDCOLOR=red
+echo "Not implemented" | aosd_cat -n "Sans 20 bold" -o 3000 -R $OSDCOLOR -f 0
+
+# End of script
