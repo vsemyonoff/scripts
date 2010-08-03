d82d0ed002a75abe30f3bb46f7e31d91cb3cfd50
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-08-03 17:15:30 +0300

    OSD Mixer and Google earth wrapper added

---
+++ mixer-osd.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#
+#  File: volume.sh
+#  Description:
+#
+#  Creation date: 2010.08.03 16:45:13
+#  Last modified: 2010.08.03 16:45:58
+#
+#  Copyright © 2010 Vladyslav Semyonoff <vsemyonoff@gmail.com>
+#
+
+case $1 in
+
+   volup) A="VOLUME: $(amixer sset Master 2dB+ | grep "Mono:" \
+      | awk '{print $4}' | tr -d '[]')" ;;
+
+   voldown) A="VOLUME: $(amixer sset Master 1dB- | grep "Mono:" \
+      | awk '{print $4}' | tr -d '[]')" ;;
+
+   mute)
+      case $(amixer set Master toggle | grep "Mono:" \
+         | awk '{print $6}' | tr -d '[]') in
+            on) A="UNMUTED" ;;
+            off) A="MUTED" ;;
+      esac ;;
+
+   *) echo "Usage: $0 { volup | voldown | mute }" ;;
+
+esac
+
+MUTESTATUS=$(amixer get Master | grep "Mono:" | awk '{print $6}' | tr -d '[]')
+
+if [ $MUTESTATUS == "off" ]; then
+   OSDCOLOR=red; else
+   OSDCOLOR=yellow
+fi
+
+killall aosd_cat &> /dev/null
+
+echo "$A" | aosd_cat -n "Sans 20 bold" -o 3000 -R $OSDCOLOR -f 0
+
+# End of script
