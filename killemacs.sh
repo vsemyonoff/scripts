f406476c48901cd3cef18a0501fab28c7c8ce023
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-08-26 12:39:32 +0300

    Boot time emacs daemon stopping script added

--- killemacs.sh
+++ killemacs.sh
@@ -4,10 +4,11 @@
 #  File: killemacs.sh
 #  Description: Save all buffers and stop emacs daemon
 #
-#  Version: killemacs.sh, 2010/08/26 12:11:15 EEST
+#  Version: killemacs.sh, 2010/08/26 12:35:15 EEST
 #  Copyright © 2010 Vladyslav Semyonov <vsemyonoff@gmail.com>
 #
 
-emacsclient --eval "(progn (save-some-buffers t) (setq kill-emacs-hook 'nil) (kill-emacs))"
+emacsclient --eval "(progn (save-some-buffers t) (setq kill-emacs-hook 'nil) (kill-emacs))" &>/dev/null && \
+    rm -rf "/tmp/emacs`id -u`"
 
 # End of killemacs.sh
