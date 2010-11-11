d0b1bafccf4ff31a74afd43ea2dffba4b83196ea
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-11-11 15:25:46 +0200

    Revert "Gentoo installation script updated"
    This reverts commit 564c7b28872f434c41b3ce436d8941a5b1fc8cb8.

--- update.sh
+++ update.sh
@@ -9,16 +9,6 @@ eselect profile set "default/linux/amd64/10.0/no-multilib"
 # Override emerge options from make.conf
 export EMERGE_DEFAULT_OPTS=""
 
-# Mask 2.6.36 kernel
-mkdir -p /etc/portage/package.mask
-echo ">=sys-kernel/gentoo-sources-2.6.36" >> /etc/portage/package.mask/gentoo-sources
-echo ">=sys-kernel/linux-headers-2.6.36" >> /etc/portage/package.mask/linux-headers
-
-# Use latest 2.6.35 kernel
-mkdir -p /etc/portage/package.keywords
-echo "sys-kernel/gentoo-sources           ~amd64" >> /etc/portage/package.keywords/gentoo-sources
-echo "sys-kernel/linux-headers            ~amd64" >> /etc/portage/package.keywords/linux-headers
-
 locale-gen && \
 emerge --sync && \
 emerge --oneshot portage && \
