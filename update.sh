22852254dfd2bbaad8895e4bf533691e131759e0
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-11-23 11:39:56 +0200

    Gentoo update script fixed

--- update.sh
+++ update.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
 
-env-update && \
-source /etc/profile
+# Update environment variables
+source /etc/profile && env-update
 
 # Change default profile
-eselect profile set "default/linux/amd64/10.0/no-multilib"
+#eselect profile set "default/linux/amd64/10.0/no-multilib"
 
-# Override emerge options from make.conf
+# Override default emerge options from make.conf
 export EMERGE_DEFAULT_OPTS=""
 
-locale-gen && \
-emerge --sync && \
-emerge --oneshot portage && \
-emerge --oneshot linux-headers glibc binutils gcc-config gcc && \
-binutils-config $(binutils-config -l | wc -l) && \
-gcc-config $(gcc-config -l | wc -l) && \
-source /etc/profile && \
-emerge --oneshot -b glibc binutils gcc && \
-echo "Toolchain update complete." > upd-"`date +%Y%m%d`".log || exit 1
+locale-gen
 
-[ $? == 0 ] && \
-emerge -bke system && \
-emerge -bke world && \
-emerge --depclean && \
-echo "World update complete." >> upd-"`date +%Y%m%d`".log || exit 1
+if [ "$1" == "--toolchain" ]; then
+    emerge --oneshot portage && \
+    emerge --oneshot linux-headers glibc binutils gcc-config gcc && \
+    binutils-config $(binutils-config -l | wc -l) && \
+    gcc-config $(gcc-config -l | wc -l) && \
+    source /etc/profile && \
+    emerge --oneshot -b glibc binutils gcc || exit 1
+    echo "Toolchain update complete." > "${HOME}/update.log"
+fi
 
-echo "Update complete. Use 'dispatch-conf' to update configs." >> upd-"`date +%Y%m%d`".log
+emerge -bke system && emerge -bke world || exit 1
+echo "World update complete." >> "${HOME}/update.log"
