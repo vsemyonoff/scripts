cf54ba405b6d2fa916e87e48f60be5b54ff1508e
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-11-25 13:19:56 +0200

    Firefox wrapper updated

--- firefox.sh
+++ firefox.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-exec bin32 /usr/bin/firefox "$@"
+FIREPATH="${HOME}/.mozilla/firefox/profiles.ini"
+PROFILE=$(grep Path ${FIREPATH} | cut -f 2 -d '=')
+rm -fr "${HOME}/.mozilla/firefox/${PROFILE}/compatibility.ini"
+exec /usr/bin/firefox "$@"
