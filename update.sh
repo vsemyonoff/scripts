4e52302d674817425bd41e208061f67c37088ef4
Vladyslav Semyonov <vsemyonoff@gmail.com>
2010-12-21 15:44:47 +0200

    System update script fixed

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Update environment variables
-source /etc/profile && env-update
-
-# Change default profile
-#eselect profile set "default/linux/amd64/10.0/no-multilib"
+env-update && source /etc/profile
 
 # Override default emerge options from make.conf
 export EMERGE_DEFAULT_OPTS=""
@@ -13,11 +10,11 @@ locale-gen
 
 if [ "$1" == "--toolchain" ]; then
     emerge --oneshot portage && \
-    emerge --oneshot linux-headers glibc binutils gcc-config gcc && \
-    binutils-config $(binutils-config -l | wc -l) && \
-    gcc-config $(gcc-config -l | wc -l) && \
-    source /etc/profile && \
-    emerge --oneshot -b glibc binutils gcc || exit 1
+        emerge --oneshot linux-headers glibc binutils gcc-config gcc && \
+        binutils-config $(binutils-config -l | wc -l) && \
+        gcc-config $(gcc-config -l | wc -l) && \
+        source /etc/profile && \
+        emerge --oneshot -b glibc binutils gcc || exit 1
     echo "Toolchain update complete." > "${HOME}/update.log"
 fi
 
