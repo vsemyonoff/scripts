1c292c170ad1796af4c6202d6ce31d85ba3d41e3
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-01-15 00:03:06 +0200

    Revert "Emacs daemon handler init script adopted to Gentoo"
    This reverts commit 37107c7520d3281eedbf5c787832677991f8e007.

--- emacsd.sh
+++ emacsd.sh
@@ -1,24 +1,34 @@
-#!/sbin/runscript
+#!/usr/bin/env bash
 
 #
 #  File: emacsd.sh
-#  Description: system service to stop all emacs daemons
+#  Description:
 #
-#  Version: emacsd.sh, 2010/12/13 02:00:06 EET
+#  Version: emacsd.sh, 2010/08/26 12:38:37 EEST
 #  Copyright © 2010 Vladyslav Semyonov <vsemyonoff@gmail.com>
 #
 
-start() {
-    ebegin "Starting Emacs daemon handler"
-    eend ${?}
-}
+. /etc/rc.conf
+. /etc/rc.d/functions
 
-stop() {
-    ebegin "Stopping Emacs daemon instances"
-    exec find /tmp -maxdepth 1 -type d -name 'emacs*' -exec find {} -type s -name server \; | \
-        perl -ne 's/[^\d]//g; print scalar getpwuid($_), "\n"' | \
-        xargs -n 1 su -c /mnt/share/scripts/killemacs.sh -
-    eend ${?}
-}
+case "$1" in
+    start)
+        add_daemon emacsd
+        ;;
+    stop)
+        stat_busy "Stopping emacs daemon"
+        exec find /tmp -maxdepth 1 -type d -name 'emacs*' -exec find {} -type s -name server \; | \
+            perl -ne 's/[^\d]//g; print scalar getpwuid($_), "\n"' | \
+            xargs -n 1 su -c /mnt/share/bin/killemacs -
+        stat_done
+        rm_daemon emacsd
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "usage: $0 {start|stop|restart}"
+esac
 
 # End of emacsd.sh
