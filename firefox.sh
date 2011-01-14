32efa50f61755927f9c3fef6c5996732f4d0c1ff
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-01-15 00:07:03 +0200

    Switched back to ArchLinux

--- firefox.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-FIREPATH="${HOME}/.mozilla/firefox/profiles.ini"
-PROFILE=$(grep Path ${FIREPATH} | cut -f 2 -d '=')
-rm -fr "${HOME}/.mozilla/firefox/${PROFILE}/compatibility.ini"
-exec /usr/bin/firefox "$@"
