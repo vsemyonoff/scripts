683543a14cae7591d9f3d737f775d12d46b5260d
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-01-24 19:15:18 +0200

    Removed ssh helper script

--- sshlog.sh
+++
@@ -1,8 +0,0 @@
-rm -f accepted.txt failed.txt refused.txt
-FILES="auth.log auth.log.1 auth.log.2 auth.log.3 auth.log.4"
-
-for i in $FILES; do
-    grep "Accepted password" $i | tac >> accepted.txt
-    grep "Failed password" $i | tac >> failed.txt
-    grep "refused connect from" $i | tac >> refused.txt
-done
