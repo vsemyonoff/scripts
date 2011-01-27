e4b61fbbdc6ab267f04987934c721c290a3fd823
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-01-27 12:33:45 +0200

    M3U parser updated

--- parsem3u.sh
+++ parsem3u.sh
@@ -5,7 +5,7 @@
 #  Description:
 #
 #  Creation date: 2011.01.22 01:51:51
-#  Last modified: 2011.01.22 16:47:03
+#  Last modified: 2011.01.27 12:30:36
 #
 #  Copyright © 2010 Vladyslav Semyonoff <vsemyonoff@gmail.com>
 #
@@ -18,7 +18,8 @@ case "$(basename ${0})" in
     parsem3u*)
         [ -z "${1}" ] && echo "Playlist not specified" && exit 1
         [ ! -f "${1}" ] && echo "Playlist not found: ${1}" && exit 1
-        mkdir -p "${OUTDIR}" || exit 1
+        rm -fr "${OUTDIR}" || exit 1
+        mkdir "${OUTDIR}" || exit 1
         cp -f "${1}" "${PLAYLIST}"
         grep -o -P "(?<=EXTINF:0,).*(?=$)" "${PLAYLIST}" |
         while read i
@@ -26,15 +27,14 @@ case "$(basename ${0})" in
             if echo "$i" | grep "\(Мультимедиа\)" &>/dev/null ; then
                 continue
             fi
-            # TODO: replace slashes and remove CR's
-            CHANNEL=$(echo "${i}" | sed -e 's/\//_/g' | tr -d '\015')
+            CHANNEL=$(echo "${i}" | sed -e 's/\//\\/g' | sed -e 's/\ /_/g' | tr -d '\015')
             ln -sf "${PROGNAME}" "${OUTDIR}/${CHANNEL}"
         done
         ;;
 
     *)
         [ ! -f "${PLAYLIST}" ] && echo "Playlist not found: ${PLAYLIST}" && exit 1
-        CHANNEL=$(echo `basename ${0}` | sed -e 's/_/\//g' | sed 's/+/\\+/g')
+        CHANNEL=$(echo `basename ${0}` | sed -e 's/\\/\//g' | sed -e 's/_/\ /g' | sed 's/+/\\+/g')
         exec mplayer $(grep -A1 -E "EXTINF:0,${CHANNEL}" "${PLAYLIST}" | tail -n 1)
         ;;
 esac
