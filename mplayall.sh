36e635ed42bc1b645671671f4ef96215169da4dd
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-01-27 13:42:20 +0200

    Mplayer wrapper for cartoons updated

--- mplayall.sh
+++ mplayall.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #
-#  File: 00-Смотреть_все
+#  File: mplayall.sh
 #  Description:
 #
 #  Creation date: 2011.01.27 12:38:00
-#  Last modified: 2011.01.27 13:14:49
+#  Last modified: 2011.01.27 13:40:42
 #
 #  Copyright © 2011 Vladyslav Semyonoff <vsemyonoff@gmail.com>
 #
 
-mplayer -- `find . -type f | sed -e 's/\ /\\\ /g' | sort -V`
+find . -type f -print | sort -V | mplayer -playlist -
