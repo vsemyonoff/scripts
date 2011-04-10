46b88ca06a0e97cf4b962c14d4b9731077d1f5f6
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-04-11 01:40:46 +0300

    Emacs wrapper updated

--- emcl.sh
+++ emcl.sh
@@ -2,29 +2,19 @@
 
 EMACS="emacsclient --alternate-editor="
 
-# Iterate arguments
-while (( "$#" )); do
-    if [ "$1" != "--nofork" ]; then
-        EMACS_ARGS="${EMACS_ARGS} $1"
-    fi
-    case "$1" in
-        --nofork|--tty|-nw|-t)
-            NOFORK=1
-            ;;
-        *)
-            ;;
-    esac
-    shift
-done
+case "$1" in
+    --nofork)
+        NOFORK=1
+        shift
+        ;;
+    --tty|-nw|-t)
+        NOFORK=1
+        ;;
+    *)
+        ;;
+esac
 
-# Exec emacs
-if [ -z ${DISPLAY} ]; then
-    exec ${EMACS} --tty ${EMACS_ARGS}
-else
-    EMACS="${EMACS} --create-frame"
-    if [ -z "${NOFORK}" ]; then
-        exec ${EMACS} ${EMACS_ARGS} &>/dev/null &
-    else
-        exec ${EMACS} ${EMACS_ARGS}
-    fi
-fi
+[ -z "${DISPLAY}" ] && FRAME="--tty" || FRAME="--create-frame"
+[ -z "${NOFORK}" ] && FORKARGS=">/dev/null 2>&1 &"
+
+eval 'exec ${EMACS} ${FRAME} "$@" ${FORKARGS}'
