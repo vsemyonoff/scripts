9809a03b9ba8ab350ff2cb6e128429dc13c54df2
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-06-05 15:49:39 +0300

    KVM started updated

--- kvmrun.sh
+++ kvmrun.sh
@@ -1,22 +1,57 @@
 #!/usr/bin/env bash
 
-IMAGEDIR="/mnt/share/images/kvmimages"
+IMAGESDIR="/mnt/share/images/kvmimages"
 IMAGENAME="$(basename ${0})"
-IMAGE="${IMAGEDIR}/${IMAGENAME}.img"
+IMAGEFULL="${IMAGESDIR}/${IMAGENAME}.img"
+CPUMODEL="host" # use 'qemu[-kvm] -cpu ?' for details
+CPUCOUNT="1"
+NUMCORES="2"
+RAMSIZE=2048
+VGAMODE="std"
 
+# Validate symlink name
 [ "${IMAGENAME}" == "kvmrun.sh" ] && echo "error: create image named symlink for this script to use it, exiting..." && exit 1
 
-if [ ! -r "${IMAGE}" ]; then
+# To be sure that images dir exists
+mkdir -p "${IMAGESDIR}" || exit 1
+
+# Validate virtual image existance
+if [ ! -r "${IMAGEFULL}" ]; then
     if [ ! -z "${1}" ]; then
-        qemu-img create -f qcow2 "${IMAGE}" 5G
+        echo -n "Enter virtual image size (gigabytes, default: 5): " && read IMAGESIZE
+        qemu-img create -f qcow2 "${IMAGEFULL}" ${IMAGESIZE:-5}G || exit 1
     else
         echo "error: image named '${IMAGENAME}' not exists, exiting..." && exit 1
     fi
 fi
 
-[ ! -z "${1}" ] && CDIMAGE="-cdrom ${1}"
+# Drive C
+DRIVE_C="-drive file=\"${IMAGEFULL}\",if=virtio,boot=on"
 
+# Drives A, D and E (use premounted folder, cdrom ".iso" and floppy ".vfd")
+for i in $(seq 1 3); do
+    ARG=$(eval echo \${${i}})
+    [ -z "${ARG}" ] && break
+    [ -d "${ARG}" ] && [ -z "${DRIVE_D}" ] && DRIVE_D="-hdd fat:\"${ARG}\""
+    if [ -f "${ARG}" ]; then
+        [[ "${ARG}" =~ (.*)\.vfd$ ]] && [ -z "${FLOPPY}" ] && FLOPPY="-fda \"${ARG}\""
+        [[ "${ARG}" =~ (.*)\.iso$ ]] && [ -z "${CDROM}" ] && CDROM="-cdrom \"${ARG}\""
+    fi
+    unset ARG
+done
+
+# Full QEMU command
+QEMUCMD="qemu-kvm -full-screen -enable-kvm \
+    -cpu ${CPUMODEL} -smp ${CPUCOUNT},cores=${NUMCORES} \
+    ${FLOPPY} ${DRIVE_C} ${DRIVE_D} ${CDROM} -boot order=cda \
+    -net nic,model=virtio,vlan=1 \
+    -net user,vlan=1 \
+    -m ${RAMSIZE} -vga ${VGAMODE}"
+
+# Shutdown 'xcompmgr'
 XCOMPMGR_PID=$(pgrep -U ${UID} "xcompmgr")
 [ ! -z "${XCOMPMGR_PID}" ] && kill -9  ${XCOMPMGR_PID}
+unset XCOMPMGR_PID
 
-exec qemu-kvm -full-screen -alt-grab -enable-kvm -drive file="${IMAGE}",if=virtio,boot=on -net nic,model=virtio,vlan=1 -net user,vlan=1 -m 1024 -vga std ${CDIMAGE}
+# Exec QEMU
+exec $(eval echo "${QEMUCMD}")
