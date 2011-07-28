02717e8bcf499530c04a319e2f8db05db78a2d52
Vladyslav Semyonov <vsemyonoff@gmail.com>
2011-07-28 19:38:47 +0300

    OSD font fixed

--- echo-osd.sh
+++ echo-osd.sh
@@ -3,16 +3,16 @@
 BG=${OSD_BG_COL:-black}
 ER=${OSD_ER_COL:-red}
 FG=${OSD_FG_COL:-white}
-FN=${OSD_FN:-"Sans 14"}
+FN=${OSD_FN:-"sans 14"}
 
-COLOR=${FG}
+CL=${FG}
 for i in ${@}; do
     case ${i} in
-        -e|--error) COLOR=${ER} ;;
+        -e|--error) CL=${ER} ;;
         *) ARGS="${ARGS} ${i}" ;;
     esac
     shift
 done
 
 killall -u ${USER} aosd_cat >/dev/null 2>&1
-echo "${ARGS}" | exec aosd_cat -d 5 -n ${FN} -s 0 -b 100 -B ${BG} -R ${COLOR}
+echo "${ARGS}" | exec aosd_cat -d 5 -n "${FN}" -s 0 -b 100 -B ${BG} -R ${CL}
